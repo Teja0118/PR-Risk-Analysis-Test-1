80f0141a5d20aeb977e1effe746d88d98dd0e4f9
Teja0118 <157273408+Teja0118@users.noreply.github.com>
2026-03-10 12:36:02 +0530

    feat: Multiply function with corresponding unit test

---
+++ README.md
@@ -0,0 +1,1 @@
+This repo is for checking the working of PR Risk Analysis Application
--- src/calculator.py
+++ src/calculator.py
@@ -3,3 +3,7 @@ def add(a, b):
 
 def subract(a, b):
     return a - b
+
+def multiply(a, b):
+    return a * b
+
